a22851b4c3ed4ed0c94401dbb510364747d4f938
Steve Zhu <ziyaozhu@connect.hku.hk>
2021-10-06 16:44:22 +0000

    change preempt logic

--- tut/src/act/fibo_actclient.py
+++ tut/src/act/fibo_actclient.py
@@ -5,50 +5,56 @@ import actionlib
 from tut.msg import *
 
 
-def feedbackCallback(feedback):
-    ros.loginfo(f"Index: {feedback.current_index}; Value: {feedback.current_value}")
+class FiboClient:
+    def __init__(self):
+        # init action client
+        self.actClient = actionlib.SimpleActionClient('/fibonacci', FibonacciAction)
+
+        # init messages
+        self.preempt_key = 0
+        self.goal = FibonacciGoal()
+
+        # Wait for action server
+        ros.loginfo("Waiting for action client")
+        self.actClient.wait_for_server()
+        ros.loginfo("Action server connected")
 
 
-def activeCallback():
-    ros.loginfo("Action started")
+    def feedbackCallback(self, feedback):
+        ros.loginfo(f"Index: {feedback.current_index}; Value: {feedback.current_value}")
+
+        if (feedback.current_value == self.preempt_key):
+            ros.logwarn("Reached preempt key, cancling action")
+            self.actClient.cancel_goal()
 
 
-def doneCallback(status, result):
-    ros.loginfo(f"Target value = {result.target_value}\n")
+    def activeCallback(self):
+        ros.loginfo("Action started")
+
+
+    def doneCallback(self, status, result):
+        ros.loginfo(f"Target value = {result.target_value}\n")
+
+
+    def sendGoal(self, target_index, preempt_key):
+        self.goal.target_index = target_index
+        self.preempt_key = preempt_key
+        self.actClient.send_goal(self.goal, feedback_cb=self.feedbackCallback, active_cb=self.activeCallback, done_cb=self.doneCallback)
 
 
 def main():
     ros.init_node("fibo_actclient")
+    ros.sleep(1)
 
-    # Init action goal message
-    goal = FibonacciGoal()
-
-    # Init action client
-    actClient = actionlib.SimpleActionClient('/fibonacci', FibonacciAction)
-
-    # Wait for action server
-    ros.loginfo("Waiting for action client")
-    actClient.wait_for_server()
-    ros.loginfo("Action server connected")
+    fiboClient = FiboClient()
 
     while (not ros.is_shutdown()):
-        ros.loginfo("Please input target index and wait time:")
+        ros.loginfo("Please input target index and preempt key:")
         try:
-            goal.target_index, wait_time = map(int, input().split())
+            fiboClient.sendGoal(*map(int, input().split()))
         except (ValueError):
-            continue;
+            continue
 
-        actClient.send_goal(goal, feedback_cb=feedbackCallback, active_cb=activeCallback, done_cb=doneCallback)
-
-        # wait_for_resul(duration) blocks until this goal finishes or timeout
-        # returns finished ? true : false
-        finished_before_timeout = actClient.wait_for_result(ros.Duration(wait_time))
-
-        if (not finished_before_timeout):
-            actClient.cancel_goal()
-            ros.logwarn("Action timeout")
-
-        ros.sleep(0.5);
 
 if (__name__ == "__main__"):
     main()
